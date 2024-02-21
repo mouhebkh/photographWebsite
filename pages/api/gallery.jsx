186eef36b10543de7ee48a9019e3ee86c8c2ff39
Hammad Younas <m.hammadyounas@outlook.com>
2024-02-21 19:57:49 +0500

    Versioning untracked files

--- pages/api/gallery.jsx
+++ pages/api/gallery.jsx
@@ -4,6 +4,7 @@ export default async function galleryHandler(req, res) {
   try {
     let nextCursor = null;
     let allImages = [];
+
     do {
       // Fetch data for the current page
       const result = await fetchGalleryData(nextCursor);
@@ -16,6 +17,8 @@ export default async function galleryHandler(req, res) {
       // Update nextCursor for the next page
       nextCursor = newNextCursor;
     } while (nextCursor); // Continue fetching until nextCursor is null
+
+    //   console.log("Fetched all images:", allImages);
 
     res.status(200).json(allImages);
   } catch (error) {
